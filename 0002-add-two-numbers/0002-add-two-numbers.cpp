7a6a4a083ff1aafeb1d64b2df6435df698e28306
abhishek-hk-93 <hk.abhishek.1993@gmail.com>
2025-01-05 22:02:31 +0530

    Time: 0 ms (100%), Space: 77.1 MB (82.13%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,7 +10,23 @@
  */
 class Solution {
 public:
+    ListNode* addRecursively(ListNode* l1, ListNode* l2, int carry=0){
+        if(!l1 && !l2 && !carry) return 0;
+
+        int num1 = l1 ? l1->val : 0;
+        int num2 = l2 ? l2->val : 0;
+        int sum = num1 + num2 + carry;
+        int digit = sum % 10;
+        carry = sum / 10;
+        ListNode* newNode = new ListNode(digit);
+        newNode->next = addRecursively(l1 ? l1->next : l1, l2 ? l2->next : l2, carry);
+        return newNode;
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // recursive
+        return addRecursively(l1, l2);
+        // Iterative
+        /*
         ListNode* temp = new ListNode(-1);
         ListNode* sumList = temp;
 
@@ -28,5 +44,6 @@ public:
             l2 = l2 ? l2->next : NULL;
         }
         return sumList->next;
+        */
     }
 };
