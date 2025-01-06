75357f296a00f91099f77128e180425f112a143f
abhishek-hk-93 <hk.abhishek.1993@gmail.com>
2025-01-06 22:08:25 +0530

    Time: 0 ms (100%), Space: 77 MB (81.69%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,27 +10,12 @@
  */
 class Solution {
 public:
-    ListNode* addRecursively(ListNode* l1, ListNode* l2, int carry=0){
-        if(!l1 && !l2 && !carry) return 0;
-
-        int num1 = l1 ? l1->val : 0;
-        int num2 = l2 ? l2->val : 0;
-        int sum = num1 + num2 + carry;
-        int digit = sum % 10;
-        carry = sum / 10;
-        ListNode* newNode = new ListNode(digit);
-        newNode->next = addRecursively(l1 ? l1->next : l1, l2 ? l2->next : l2, carry);
-        return newNode;
-    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // recursive
-        return addRecursively(l1, l2);
-        // Iterative
-        /*
-        ListNode* temp = new ListNode(-1);
-        ListNode* sumList = temp;
-
+        // iterative
+        ListNode* sumList = new ListNode(-1);
+        ListNode* temp = sumList;
         int carry = 0;
+
         while(l1 || l2 || carry){
             int num1 = l1 ? l1->val : 0;
             int num2 = l2 ? l2->val : 0;
@@ -43,7 +28,6 @@ public:
             l1 = l1 ? l1->next : NULL;
             l2 = l2 ? l2->next : NULL;
         }
-        return sumList->next;
-        */
+    return sumList->next;
     }
 };
