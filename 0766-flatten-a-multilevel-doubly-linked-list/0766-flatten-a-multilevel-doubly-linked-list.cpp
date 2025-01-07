2ab2d9fd1531872aa5ed8f226ffce9e35d687a9a
abhishek-hk-93 <hk.abhishek.1993@gmail.com>
2025-01-07 20:28:00 +0530

    Time: 3 ms (65.28%), Space: 11.6 MB (60.16%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
@@ -11,26 +11,27 @@ public:
 
 class Solution {
 public:
-    Node* getChildTail(Node* node){
+    Node* flattenWithChild(Node* node){
         if(!node) return nullptr;
         Node* temp = node;
         Node* childTail = temp;
         while(temp){
             if(temp->child){
-                childTail = getChildTail(temp->child);
+                childTail = flattenWithChild(temp->child);
                 childTail->next = temp->next;
                 if(temp->next) temp->next->prev = childTail;
                 temp->next = temp->child;
                 temp->next->prev = temp;
                 temp->child = nullptr;
             }
+            // always keep track of prev to temp, required when temp becomes null
             childTail = temp;
             temp = temp->next;
         }
         return childTail;
     }
     Node* flatten(Node* head) {
-        getChildTail(head);
+        flattenWithChild(head);
         return head;
     }
 };
